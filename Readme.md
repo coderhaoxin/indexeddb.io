2746659a5fa0c0a0e7b7aa7ceda1f421945e6b9c
haoxin <coderhaoxin@outlook.com>
2015-09-06 11:24:18 +0800

    v0.1.0 - init

---
+++ Readme.md
@@ -0,0 +1,20 @@
+
+[![NPM version][npm-img]][npm-url]
+[![License][license-img]][license-url]
+[![Dependency status][david-img]][david-url]
+
+### indexeddb.io
+
+* Promise based API wrapper for indexeddb.
+
+### License
+MIT
+
+[npm-img]: https://img.shields.io/npm/v/storage.io.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/storage.io
+[travis-img]: https://img.shields.io/travis/onebook/storage.io.svg?style=flat-square
+[travis-url]: https://travis-ci.org/onebook/storage.io
+[license-img]: https://img.shields.io/badge/license-MIT-green.svg?style=flat-square
+[license-url]: http://opensource.org/licenses/MIT
+[david-img]: https://img.shields.io/david/onebook/storage.io.svg?style=flat-square
+[david-url]: https://david-dm.org/onebook/storage.io
---
+++ test/index.js
@@ -0,0 +1,85 @@
+'use strict'
+
+import { expect } from 'chai'
+import Store from '../'
+
+describe('## indexeddb.io', () => {
+  describe('# basic', () => {
+    const store = new Store({
+      db: 'test',
+      store: {
+        name: 'item',
+        keyPath: 'id',
+        autoIncrement: true
+      },
+      indexes: [{
+        name: 'name',
+        property: 'name',
+        unique: true
+      }]
+    })
+
+    let id
+
+    it('init', () => {
+      return store.init()
+    })
+
+    it('add', () => {
+      return store.add({
+        name: 'haoxin',
+        desc: 'hello'
+      })
+      .then((result) => {
+        id = result
+      })
+    })
+
+    it('get', () => {
+      return store.get(id)
+        .then((result) => {
+          expect(result.name).to.equal('haoxin')
+        })
+    })
+
+    it('put', () => {
+      return store.put({
+        id: id,
+        name: 'haoxin',
+        desc: 'hello world'
+      })
+    })
+
+    it('get', () => {
+      return store.get(id)
+        .then((result) => {
+          expect(result.name).to.equal('haoxin')
+        })
+    })
+
+    it('findOne', () => {
+      return store.findOne('name', 'haoxin')
+        .then((result) => {
+          expect(result.name).to.equal('haoxin')
+        })
+    })
+
+    it('find', () => {
+      return store.find('name')
+        .then((cursor) => {
+          expect(cursor.value.name).to.equal('haoxin')
+        })
+    })
+
+    it('find with keyRange', () => {
+      return store.find('name', IDBKeyRange.only('haoxin'))
+        .then((cursor) => {
+          expect(cursor.value.name).to.equal('haoxin')
+        })
+    })
+
+    it('remove', () => {
+      return store.remove(id)
+    })
+  })
+})
